943b867bf829fd10b7ed48b5d1b5bae725818b84
skmyj <131327419+skmyj@users.noreply.github.com>
2024-10-08 10:22:45 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ console.log('Hello world!');
 const btnA = document.querySelector('#btn-a');
 const btnB = document.querySelector('#btn-b');
 const btnC = document.querySelector('#btn-c');
+const btnD = document.querySelector('#btn-d');
 
 btnA.addEventListener('click', () => {
   document.documentElement.setAttribute('data-theme','color-a');
@@ -15,4 +16,8 @@ btnB.addEventListener('click', () => {
 
 btnC.addEventListener('click', () => {
   document.documentElement.setAttribute('data-theme','color-c');
+});
+
+btnD.addEventListener('click', () => {
+  document.documentElement.setAttribute('data-theme','color-d');
 });
